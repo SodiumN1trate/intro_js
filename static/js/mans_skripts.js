744a881ae07d8a4ae269820effe63223e6416351
SodiumN1trate <renarsgausins21@gmail.com>
2021-03-02 14:13:09 +0200

    Pievienots JS

---
+++ static/js/mans_skripts.js
@@ -0,0 +1,9 @@
+console.log("Viss strādā!")
+//alert("Hello")
+alert("Māris!")
+
+let mainigaisTeksts = "skola"
+let mainigaisSkaitlis = 7
+
+console.log(mainigaisSkaitlis)
+console.log(mainigaisTeksts)
